02fa7cfd678cafc490c96b17f72736997a8e7094
Luca Tringali <tringalinvent@libero.it>
2019-01-06 01:07:28 +0100

    Crea la cartella per i risultati al download

--- download-informaggio.sh
+++ download-informaggio.sh
@@ -4,4 +4,5 @@ rm latest_informaggio_csv.zip
 wget http://www.zorbaproject.org/informaggio/latest_informaggio_csv.zip
 rm -r ./latest_informaggio_csv
 mkdir latest_informaggio_csv
+mkdir latest_informaggio_csv/risultati
 unzip latest_informaggio_csv.zip -d latest_informaggio_csv 
