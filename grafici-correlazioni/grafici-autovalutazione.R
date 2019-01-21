eb73e2557893c44249421df93b7aebf479cedfcf
Luca Tringali <tringalinvent@libero.it>
2019-01-21 14:55:54 +0100

    Scale discrete di colori

--- grafici-correlazioni/grafici-autovalutazione.R
+++ grafici-correlazioni/grafici-autovalutazione.R
@@ -1,13 +1,16 @@
 #!/usr/bin/Rscript
+library(ggplot2);
+require(gridSVG);
 library(reshape2);
-imported <- read.csv (file="/home/flo/Progetti/informaggio/grafici-correlazioni/punteggi_per_autovalutazione.csv",  na.strings = "NA", nrows = -1, skip = 0, check.names = TRUE, strip.white = FALSE, blank.lines.skip = TRUE);
+
+imported <- read.csv (file="./punteggi_per_autovalutazione.csv",  na.strings = "NA", nrows = -1, skip = 0, check.names = TRUE, strip.white = FALSE, blank.lines.skip = TRUE);
 my.csv.data <- imported;
 my.csv.melted <- melt(my.csv.data, id="X0");
 grafico1 = ggplot(my.csv.melted, aes(x=variable, y=value, fill=factor(X0))) + geom_bar(stat="identity", width=1);
 grafico1 = grafico1 + scale_fill_discrete();
 grafico1 = grafico1 + labs(x = NULL, y = NULL, fill = NULL);
 print(grafico1);
-grid.export("/home/flo/Progetti/informaggio/punteggi_per_autovalutazione1.svg",addClasses=TRUE);
+grid.export("./punteggi_per_autovalutazione1.svg",addClasses=TRUE);
 
 my.csv.data2 <- my.csv.data
 my.csv.data2$X0 <- my.csv.data$X0
@@ -21,5 +24,15 @@ my.csv.melted2 <- melt(my.csv.data2, id="X0");
 grafico2 = ggplot(my.csv.melted2, aes(x=variable, y=value, fill=factor(X0))) + geom_bar(stat="identity", width=1);
 grafico2 = grafico2 + scale_fill_discrete();
 grafico2 = grafico2 + labs(x = NULL, y = NULL, fill = NULL);
+grafico2 = grafico2 + theme_classic() + theme(axis.line = element_blank(),
+            axis.text.y = element_blank(),
+            axis.text.x = element_text(angle = 270, hjust = 1),
+            axis.ticks = element_blank(),
+            plot.title = element_text(hjust = 0.5, color = "#666666"));
+#Ribalto il grafico e imposto il rapporto altezza/larghezza
+grafico2 = grafico2 + coord_flip();
+grafico2 = grafico2 + coord_fixed(ratio = 0.1);
+#Imposto il numero delle colonne in cui viene divisa la legenda
+grafico2 = grafico2 + guides(fill=guide_legend(ncol=8))
 print(grafico2);
-grid.export("/home/flo/Progetti/informaggio/grafici-correlazioni/punteggi_per_autovalutazione2.svg",addClasses=TRUE);
+grid.export("./punteggi_per_autovalutazione2.svg",addClasses=TRUE);
--- grafici-correlazioni/grafici-categorie.R
+++ grafici-correlazioni/grafici-categorie.R
@@ -5,8 +5,12 @@ require(gridSVG);
 
 
 path <- "../latest_informaggio_csv/risultati/";
-categorie <- c("elaborazioneinformazioni", "creazionecontenuti", "comunicazione", "risoluzioneproblemi","sicurezza");
 euroVal = c("base", "medio", "avanzato", "nessuno", "tutto");
+categorie <- c("elaborazioneinformazioni", "creazionecontenuti", "comunicazione", "risoluzioneproblemi","sicurezza");
+#Valori minimi e massimi per ciascuna categoria
+catMax <- c(8,5,3,2.5,5.5);
+catMin <- c(-12,-10,-3,-2.5,-6.5);
+
 
 
 for(ic in 1:length(categorie)){
@@ -15,6 +19,10 @@ tutti <- read.table(text = "", col.names=c("Risposte" , "Utenti"), colClasses = 
 for(iv in 1:length(euroVal)){
 filename <- paste(path,"quiz-", categorie[ic], "=", euroVal[iv], "-", categorie[ic],".csv", sep="");
 file <- read.table(filename,header=FALSE, sep=",", col.names=c("Risposte" , "Utenti"), colClasses = c("numeric", "numeric"));
+
+#Aggiungo i valori minimi e massimi come punti di riferimento
+tutti[nrow(tutti) + 1,] = list(catMin[ic], 0);
+tutti[nrow(tutti) + 1,] = list(catMax[ic], 0);
 
 for(it in 1:length(file$Risposte)){
 if (is.element(file$Risposte[it], tutti$Risposte)) {
@@ -27,17 +35,22 @@ tutti[nrow(tutti) + 1,] = list(file$Risposte[it], file$Utenti[it]);
 #print(categorie[ic])
 #print(tutti)
 
+#Calcolo le percentuali
 tutti$Utenti <- (tutti$Utenti*100)/sum(tutti$Utenti);
+#Ordino in base al punteggio
+tutti <- tutti[order(tutti$Risposte),];
+tutti$Risposte <- as.factor(tutti$Risposte);
 
 histogram = ggplot(tutti, aes(x=tutti$Risposte, y=tutti$Utenti, fill=tutti$Risposte)) + geom_bar(stat="identity");
-mylabels = tutti$Utenti;
-#histogram = histogram + stat_count(aes(y=..count..,label=mylabels),geom="text",vjust=-1)
+mylabels = paste0(as.integer(tutti$Utenti), "%");
+histogram = histogram + stat_count(aes(y=..count..,label=mylabels),geom="text",vjust=-1)
+#histogram = histogram + scale_x_discrete(labels=tutti$Risposte);
 histogram = histogram + labs(x = NULL, y = NULL, fill = NULL, title = categorie[ic]);
 histogram = histogram + theme_classic() + theme(axis.line = element_blank(),
-            axis.text = element_blank(),
+            axis.text.y = element_blank(),
             axis.ticks = element_blank(),
             plot.title = element_text(hjust = 0.5, color = "#666666"));
 #Esporto il grafico in un file SVG
 print(histogram);
-grid.export(paste("/home/flo/Progetti/informaggio/grafici-correlazioni/", categorie[ic], ".svg", sep=""),addClasses=TRUE);
+grid.export(paste("./", categorie[ic], ".svg", sep=""),addClasses=TRUE);
 }
